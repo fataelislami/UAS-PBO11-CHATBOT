c145a03d3ac301e8ae914ff8efc358766fbc6f84
fataelislami <santribloggers@gmail.com>
2018-02-03 13:59:10 +0700

    Create ClientAPI

---
+++ src/main/java/Controller/ClientAPI.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ClientAPI {
+
+    private static final String API_BASE_URL="https://islamify.id/";
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+}
