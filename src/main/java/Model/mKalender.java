d522af9f5a26e4b716e9df8a815b9b6ded77b0d0
Unknown <sitanggangf2@gmail.com>
2018-02-03 17:25:49 +0700

    Add model
    model untuk kalender

---
+++ src/main/java/Model/mKalender.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class mKalender {
+
+    private Integer code;
+    private String hijriyah;
+    private String masehi;
+    private String  status;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getHijriyah() {
+        return hijriyah;
+    }
+
+    public String getMasehi() {
+        return masehi;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
