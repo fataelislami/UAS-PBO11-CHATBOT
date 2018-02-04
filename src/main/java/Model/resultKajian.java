bb6f801a2133f4f8dd7b4d4d8f4550f06de6de2a
Unknown <ilprasetyo2125@gmail.com>
2018-02-04 19:28:13 +0700

    add result kajian

---
+++ src/main/java/Model/resultKajian.java
@@ -0,0 +1,45 @@
+package Model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class resultKajian {
+
+    private String url;
+    private String image;
+    private String username;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
